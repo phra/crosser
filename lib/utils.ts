90dd8b98706c7ea07e18be318b16702647edc2d5
phra <greensoncio@gmail.com>
2019-05-28 00:16:46 +0200

    wip: improve encodePayload()

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,8 @@
 export function encodePayload(payload: string) {
-  return encodeURIComponent(payload).replace(/\(/g, '%28').replace(/\)/g, '%29')
+  return encodeURIComponent(payload)
+    .replace(/\(/g, '%28')
+    .replace(/\)/g, '%29')
+    .replace(/\!/g, '%21')
+    .replace(/\*/g, '%2a')
+    .replace(/\'/g, '%27')
 }

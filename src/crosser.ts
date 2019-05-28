56272b19cc02ae45abaa08b2d93ba122f82f37b2
phra <greensoncio@gmail.com>
2019-05-28 17:32:32 +0200

    build: move index.ts to src

---
+++ src/crosser.ts
@@ -0,0 +1,6 @@
+// import './crosser'
+
+export { run } from '@oclif/command'
+export { PAYLOADS } from './lib/payloads'
+export { reflectedXssScan } from './lib/reflected-xss'
+export { storedXssScan } from './lib/stored-xss'
